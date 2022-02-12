aba0af641cfaadac16caaf53ac1ea9fcd0547ef8
max <me@maxg.ca>
2022-02-11 19:41:40 -0500

    Reorganization and addition of Binds

--- src/DocObject.js
+++ src/DocObject.js
@@ -17,17 +17,17 @@ class DocElements {
 class DocObject {
     _values = {};
     _updating
-    _cached
     elements;
     render = [];
+    binds = {}
 
     set values(values){
-        throw Error("Tried to set DocObject.value. Try setting a inner object instead.")
+        throw Error("Tried to set DocObject.value. Try creating a inner object instead.")
     }
     get values(){
         return this._values;
     }
-    constructor({elements, values, render}){
+    constructor({elements, values, render, binds}){
         this.elements = new DocElements()
         if(elements){
             Object.entries(elements).forEach((e=>{this.elements[e[0]]=e[1]}))
@@ -37,30 +37,50 @@ class DocObject {
         }else{
             render = [];
         }
+        if(binds && typeof binds === 'object'){
+            this.binds = binds;
+        }
         this._values = new Proxy(!values || typeof values !== 'object' ? {} : values, {
             set: (target, prop, value, receiver) => {
-                    this.render.filter(ren => ( ren.dep && Array.isArray(ren.dep) && ren.dep.includes(prop)) || (ren.dep === undefined)).forEach(ren => {
-                        if(ren.clean) ren.clean({...this.values, [prop]:value}, this.values)
-                        ren.action({...this.values, [prop]:value}, this.values)
-                    })
+                this.runRender({[prop]:value})
                 target[prop] = value;
             }
         })
         this.onLoad = ()=>{
-            this.render.forEach(ren => {
-                if(ren.dep && Array.isArray(ren.dep) && (ren.dep.length === 0 || ren.dep.includes(true))){
-                    ren.action(this.values, this.values)
-                }
-            })
+            this.runRender({[true]:true})
         }
     }
-    runRender(...deps){
-        deps.forEach(prop=>{
-            this.render.filter(ren => ( ren.dep && Array.isArray(ren.dep) && ren.dep.includes(prop)) || (ren.dep === undefined)).forEach(ren => {
-                if(ren.clean) ren.clean({...this.values, [prop]:value}, this.values)
-                ren.action({...this.values, [prop]:value}, this.values)
-            })
+    runRender(valueChanges = {}){
+        
+        this.render.filter(ren => ( ren.dep && Array.isArray(ren.dep) && ren.dep.some((dep)=>(dep in valueChanges))) || (ren.dep === undefined)).forEach(ren => {
+            if(ren.clean) ren.clean({...this.values, ...valueChanges}, this.values)
+            ren.action({...this.values, ...valueChanges}, this.values)
         })
+        this.runBinds(valueChanges);
         
     }
+    runBinds(valueChanges = {}){
+        $('[d-bind]').toArray().forEach( e => {
+            let bind = $(e).attr('d-bind')
+            if(bind in this.binds){
+                $(e).html(this.binds[bind]({...this.values, ...valueChanges}, this.values))
+            }
+        })
+    }
 }
+
+/*
+var doc = new DocObject({
+    values: {
+    },
+    elements:{
+
+    },
+    binds:{
+
+    },
+    render: [
+
+    ]
+}); $(doc.onLoad)
+*/
