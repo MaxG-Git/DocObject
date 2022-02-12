c8993c3da17b03d24f9c45586b9d25996bd03029
max <me@maxg.ca>
2022-02-12 00:59:56 -0500

    Recursive Binds start

--- src/DocObject.js
+++ src/DocObject.js
@@ -1,14 +1,14 @@
 
 class DocElements {
-    constructor () {
+    constructor() {
         return new Proxy(this, this);
     }
-    get (target, prop) {
-        return this[prop] ? this[prop]() : $('#'+prop)
+    get(target, prop) {
+        return this[prop] ? this[prop]() : $('#' + prop)
     }
-    set(target, prop, value, receiver){
-        if(typeof value === 'string') value = $(value)
-        target[prop] = _=>value
+    set(target, prop, value, receiver) {
+        if (typeof value === 'string') value = $(value)
+        target[prop] = _ => value
         return true;
     }
 }
@@ -18,54 +18,60 @@ class DocObject {
     _values = {};
     _updating
     elements;
+    root;
     render = [];
     binds = {}
 
-    set values(values){
+    set values(values) {
         throw Error("Tried to set DocObject.value. Try creating a inner object instead.")
     }
-    get values(){
+    get values() {
         return this._values;
     }
-    constructor({elements, values, render, binds}){
-        this.elements = new DocElements()
-        if(elements){
-            Object.entries(elements).forEach((e=>{this.elements[e[0]]=e[1]}))
+    constructor({ elements, values, render, binds, root }) {
+        if (root) {
+            this.root = root;
+        } else {
+            this.root = document;
         }
-        if(render && Array.isArray(render)){
+        this.elements = new DocElements()
+        if (elements) {
+            Object.entries(elements).forEach((e => { this.elements[e[0]] = e[1] }))
+        }
+        if (render && Array.isArray(render)) {
             this.render = render;
-        }else{
+        } else {
             render = [];
         }
-        if(binds && typeof binds === 'object'){
+        if (binds && typeof binds === 'object') {
             this.binds = binds;
         }
         this._values = new Proxy(!values || typeof values !== 'object' ? {} : values, {
             set: (target, prop, value, receiver) => {
-                this.runRender({[prop]:value})
+                this.runRender({ [prop]: value })
                 target[prop] = value;
             }
         })
-        this.onLoad = ()=>{
-            this.runRender({[true]:true})
+        this.onLoad = () => {
+            this.runRender({ [true]: true })
         }
     }
-    runRender(valueChanges = {}){
-        
-        this.render.filter(ren => ( ren.dep && Array.isArray(ren.dep) && ren.dep.some((dep)=>(dep in valueChanges))) || (ren.dep === undefined)).forEach(ren => {
-            if(ren.clean) ren.clean({...this.values, ...valueChanges}, this.values)
-            ren.action({...this.values, ...valueChanges}, this.values)
+    runRender(valueChanges = {}) {
+
+        this.render.filter(ren => (ren.dep && Array.isArray(ren.dep) && ren.dep.some((dep) => (dep in valueChanges))) || (ren.dep === undefined)).forEach(ren => {
+            if (ren.clean) ren.clean({ ...this.values, ...valueChanges }, this.values)
+            ren.action({ ...this.values, ...valueChanges }, this.values)
         })
-        this.runBinds(valueChanges);
-        
+        this.runBinds(this.root, valueChanges);
     }
-    runBinds(valueChanges = {}){
-        $('[d-bind]').toArray().forEach( e => {
-            let bind = $(e).attr('d-bind')
-            if(bind in this.binds){
-                $(e).html(this.binds[bind]({...this.values, ...valueChanges}, this.values))
-            }
-        })
+    runBinds(root, valueChanges = {}) {
+        $(root).find('[d-bind]').toArray().forEach(e => {
+                let bind = $(e).attr('d-bind')
+                if (bind in this.binds) {
+                    $(e).html(this.runBinds($(this.binds[bind]({ ...this.values, ...valueChanges }, this.values)), valueChanges));
+                }
+            })
+        return root;
     }
 }
 
