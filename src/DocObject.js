d091e14c2385cb8847e9c63a0428d655c5add433
max <me@maxg.ca>
2022-02-12 16:13:21 -0500

    Added to jQeury function Object. General Clean Up

--- src/DocObject.js
+++ src/DocObject.js
@@ -1,26 +1,25 @@
-
-class DocElements {
-    constructor() {
-        return new Proxy(this, this);
-    }
-    get(target, prop) {
-        return this[prop] ? this[prop]() : $('#' + prop)
-    }
-    set(target, prop, value, receiver) {
-        if (typeof value === 'string') value = $(value)
-        target[prop] = _ => value
-        return true;
-    }
+if(!window.jQuery){
+    throw Error("JQuery is not detected. Please load JQuery before DocObject")
 }
-
-
 class DocObject {
+
+    defaults = {
+        render: [],
+        binds : {},
+        elements: {},
+        values: {},
+        bindAttr : 'd-bind'
+    }
+
+
+
     _values = {};
     _updating
     elements;
     root;
     render = [];
-    binds = {}
+    binds = {};
+    bindAttr;
 
     set values(values) {
         throw Error("Tried to set DocObject.value. Try creating a inner object instead.")
@@ -28,23 +27,25 @@ class DocObject {
     get values() {
         return this._values;
     }
-    constructor({ elements, values, render, binds, root }) {
-        if (root) {
-            this.root = root;
-        } else {
-            this.root = document;
-        }
-        this.elements = new DocElements()
+    constructor(root, options) {
+        const { elements, values, render, binds, bindAttr } = $.extend( {}, this.defaults, options );
+        this.root = $(root)[0];
+        this.root._DocObject = this;
+        if (render && Array.isArray(render)) this.render = render;
+        if (binds && typeof binds === 'object') this.binds = binds;
+        if(bindAttr) this.bindAttr = bindAttr
+        this.elements = new Proxy(!elements || typeof elements !== 'object' ? {} : elements, {
+            get: (target, prop) => {
+                return target[prop] ? target[prop]() : $(this.root).find( /.*(\.|\#|\[|\]).*/gm.exec(prop) ? prop : '#' + prop)
+            },
+            set: (target, prop, value, receiver) => {
+                if (typeof value === 'string') value = $(this.root).find(value)
+                target[prop] = _ => value
+                return true;
+            }
+        })
         if (elements) {
             Object.entries(elements).forEach((e => { this.elements[e[0]] = e[1] }))
-        }
-        if (render && Array.isArray(render)) {
-            this.render = render;
-        } else {
-            render = [];
-        }
-        if (binds && typeof binds === 'object') {
-            this.binds = binds;
         }
         this._values = new Proxy(!values || typeof values !== 'object' ? {} : values, {
             set: (target, prop, value, receiver) => {
@@ -55,7 +56,10 @@ class DocObject {
         this.onLoad = () => {
             this.runRender({ [true]: true })
         }
+        $(this.onLoad)
     }
+    
+
     runRender(valueChanges = {}) {
 
         this.render.filter(ren => (ren.dep && Array.isArray(ren.dep) && ren.dep.some((dep) => (dep in valueChanges))) || (ren.dep === undefined)).forEach(ren => {
@@ -65,15 +69,29 @@ class DocObject {
         this.runBinds(this.root, valueChanges);
     }
     runBinds(root, valueChanges = {}) {
-        $(root).find('[d-bind]').toArray().forEach(e => {
-                let bind = $(e).attr('d-bind')
+        $(root).find(`[${this.bindAttr}]`).toArray().forEach(e => {
+                let bind = $(e).attr(this.bindAttr)
                 if (bind in this.binds) {
-                    $(e).html(this.runBinds($(this.binds[bind]({ ...this.values, ...valueChanges }, this.values)), valueChanges));
+                    $(e).replaceWith(this.runBinds(($(this.binds[bind]({ ...this.values, ...valueChanges }, this.values)).attr(this.bindAttr, bind)), valueChanges));
+                    
                 }
             })
         return root;
     }
 }
+
+(function($) {
+    $.fn.extend({
+        DocObject : function( options = null) {
+            if(this[0]._DocObject && !options ) return this[0]._DocObject;
+            this.each(function() {
+                new DocObject(this, options);
+            });
+            new DocObject(this, options)
+            return this[0]._DocObject;
+        }
+    })
+})(jQuery);
 
 /*
 var doc = new DocObject({
